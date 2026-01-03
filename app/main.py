a133652695417c4269cf7ead25fb73e633b55d9f
Viktoria75 <viktoria.03.ch@gmail.com>
2026-01-03 15:46:40 +0200

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Final Project V1 - Deployed via ArgoCD"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
