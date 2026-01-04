0f005e6f675ccbfc5fa8f2887c8a9abdee4a486d
Viktoria75 <viktoria.03.ch@gmail.com>
2026-01-04 11:33:54 +0200

    Update to V2

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Final Project V1 - Deployed via ArgoCD"}
+    return {"message": "Final Project V1 - Update Successful!"}
 
 @app.get("/health")
 def health_check():
