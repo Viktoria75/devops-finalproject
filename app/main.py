746a159e3e09514b50cfcf10dc3fa10529f8bf9f
Viktoria75 <viktoria.03.ch@gmail.com>
2026-01-05 17:12:57 +0200

    Edit

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Final Project V1 - Deployed via ArgoCD"}
+    return {"message": "Final Project V1 - Added Trivy and Ingress"}
 
 @app.get("/health")
 def health_check():
